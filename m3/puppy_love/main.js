702b5848a9ca1412ebc5d22ca505ed731a338bf3
JennaConnolly <jjsurfgirl@rocketmail.com>
2015-09-17 15:29:33 -0700

    changes

---
+++ m3/puppy_love/main.js
@@ -0,0 +1,9 @@
+// $(function(){
+//    $('div').on('click', function(){
+//     $(this).remove(); 
+//     });
+//     // var parent = document.getElementById("body");
+//     // var child = document.getElementById('div');
+//     // parent.removeChild('div');
+
+// });
